6d7f2a6ac4b70aa9529e5330fb8774dc7666000d
Johan Mendez <jamg_90_@hotmail.com>
2021-04-06 22:26:28 -0500

    add clientes controller

---
+++ PruebaFacturacion/Facturacion/Controllers/ClientesController.cs
@@ -0,0 +1,30 @@
+﻿using FacturacionDatos;
+using FacturacionDatos.Context;
+using FacturacionEntidades.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facturacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        public ClientesController(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public ApplicationDbContext Context { get; }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetAsync()
+        {
+            ClientesDA clientes = new ClientesDA(Context);
+            return await clientes.GetClientesAsync().ConfigureAwait(false);
+        }
+    }
+}
