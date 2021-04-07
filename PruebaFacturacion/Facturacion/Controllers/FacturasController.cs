bf5961bc49931b10298baeac0df7d958c0900844
Johan Mendez <jamg_90_@hotmail.com>
2021-04-06 20:51:28 -0500

    update controller facturas

--- PruebaFacturacion/Facturacion/Controllers/FacturasController.cs
+++ PruebaFacturacion/Facturacion/Controllers/FacturasController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FacturacionDatos.Context;
+using FacturacionEntidades.Entities;
+using FacturacionDatos;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +9,41 @@ using System.Threading.Tasks;
 
 namespace Facturacion.Controllers
 {
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class FacturasController:ControllerBase
     {
+        public FacturasController(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public ApplicationDbContext Context { get; }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Factura>>> GetAsync()
+        {
+            FacturasDA facturasDA = new FacturasDA(Context);
+            return await facturasDA.GetFacturasAsync().ConfigureAwait(false);
+        }
+
+        [HttpGet("{id}", Name ="GetFactura")]
+        public async Task<ActionResult<Factura>> GetAsync(string id)
+        {
+            FacturasDA facturasDA = new FacturasDA(Context);
+            var factura = await facturasDA.GetFacturaByIdAsync(id: id).ConfigureAwait(false);
+            if (factura == null)
+                return NotFound();
+            return factura;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Factura factura)
+        {
+            FacturasDA facturasDA = new FacturasDA(Context);
+            if (await facturasDA.AddFacturaAsync(factura).ConfigureAwait(false))
+                return new CreatedAtRouteResult("GetFactura", new { id = factura.Id}, factura);
+            return BadRequest();
+        }
     }
 }
